ae6c6ca2e8b19c2e4b3f778f52a862cde882ac53
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-11-03 10:34:31 -0500

    created Gameboard class

--- battleship.js
+++ battleship.js
@@ -5,17 +5,21 @@ class Spaceship {
         this.firepower = 5; //The amount of damage done per attack.
         this.accuracy = .7; //chances of hitting a target;
     }
-}
+
+    attack() {
+        return this.firepower;
+    }
+} // end Spaceship class
 
 class Alien {
     constructor() {
         this.name = 'Alien';
-        this.hull = this.determineValue(3, 7);
-        this.firepower = this.determineValue(2, 5);
-        this.accuracy = this.determineValue(.6, .9);
+        this.hull = this.#determineValue(3, 7);
+        this.firepower = this.#determineValue(2, 5);
+        this.accuracy = this.#determineValue(.6, .9);
     }
 
-        determineValue(minInclusive, maxExclusive) {
+        #determineValue(minInclusive, maxExclusive) { // Private method to determine created values.
             let min;
             let max;
 
@@ -29,10 +33,42 @@ class Alien {
                 return Math.floor(Math.random() * (max - min) + min);
             }
         }
+        
+        attack() {
+            return this.firepower;
+        }
     } //end Alien class
 
-let testObj = [];
-for (let i = 0; i < 20; i++) {
-    testObj.push(new Alien());
-}
-console.log(testObj);
+    class Gameboard { //this is a class where we can perform the key functionality of the game.
+        constructor() {
+            this.name = 'Space Battle';
+            this.player = new Spaceship();
+            this.alienPlayers = this.#generateAliens();
+        }
+        
+        #generateAliens() {
+            let result = [];
+            let numberOfAliens = Math.ceil(Math.random() * 6);
+            
+            for(let i = 0; i < numberOfAliens; i++) {
+                result.push(new Alien());
+            }
+            return result;
+        }
+
+        pickTarget() {
+            return this.alienPlayers[Math.floor(Math.random() * this.alienPlayers.length)];
+        }
+        playRound(player, pickTarget) {
+            while(player.hull > 0 && pickTarget.hull > 0) {
+                return 0;
+            }
+
+        }
+
+
+    } // end Gameboard class
+
+    let game = new Gameboard();
+    console.log(game)
+    console.log(game.pickTarget())
